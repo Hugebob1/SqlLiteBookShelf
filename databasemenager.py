0e8fd4412612acabcc66965606e4753e5deecefe
Hugebob1 <01197882@pw.edu.pl>
2025-07-03 18:09:58 +0200

    SQLAlchemy creating and addind data to basic database

--- databasemenager.py
+++ databasemenager.py
@@ -1,10 +1,34 @@
-import sqlite3
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
-db = sqlite3.connect("books-collection.db")
+app = Flask(__name__)
 
-cursor = db.cursor()
+##CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
 
-#cursor.execute("CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
 
-cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.3')")
-db.commit()
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+
+class Book(db.Model):
+  id: Mapped[int] = mapped_column(Integer, primary_key=True)
+  title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+  author: Mapped[str] = mapped_column(String(250), nullable=False)
+  rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+  def __repr__(self):
+    return f'<Book {self.title}>'
+
+with app.app_context():
+  db.create_all()
+
+with app.app_context():
+  new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
+  db.session.add(new_book)
+  db.session.commit()
